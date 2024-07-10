d93a04a5cda48e632083158d40da2b5f00463448
iamnishanth <nishanthvenk68@gmail.com>
2024-07-11 04:07:39 +0530

    feat: added bottom nav bar

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,6 @@
-import { Outlet, redirect, useNavigation } from "react-router-dom";
+import { NavLink, Outlet, redirect, useNavigation } from "react-router-dom";
+
+import { CircleUserRound, Home, Squircle } from "lucide-react";
 
 import Navbar from "@/components/navbar";
 
@@ -10,9 +12,32 @@ const Layout = () => {
   const navigation = useNavigation();
 
   return (
-    <main>
+    <main className="relative min-h-[100dvh]">
       <Navbar />
       {navigation.state === "loading" ? <Loading className="h-[calc(100vh-5rem)]" /> : <Outlet />}
+      <footer className="fixed bottom-0 md:hidden h-20 w-full border-t flex items-center justify-evenly">
+        <NavLink
+          to="/"
+          className="flex flex-col items-center justify-center gap-2 text-xs w-20 text-muted-foreground [&.active]:text-foreground"
+        >
+          <Home />
+          All
+        </NavLink>
+        <NavLink
+          to="/spaces"
+          className="flex flex-col items-center justify-center gap-2 text-xs w-20 text-muted-foreground [&.active]:text-foreground"
+        >
+          <Squircle />
+          Spaces
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className="flex flex-col items-center justify-center gap-2 text-xs w-20 text-muted-foreground [&.active]:text-foreground"
+        >
+          <CircleUserRound />
+          Profile
+        </NavLink>
+      </footer>
     </main>
   );
 };
